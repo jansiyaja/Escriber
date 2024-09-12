b69722baf1201f33d377e3fdc226085c1fc1e5f7
Jansiya jahan <jansiyajahan8@gmail.com>
2024-09-13 00:16:26 +0530

    feat(error): add  invalid token error and unauhorized  error (custom error)

--- backend/frameworks/errors/BadRequestError.ts
+++ backend/frameworks/errors/BadRequestError.ts
@@ -30,3 +30,29 @@ export class  InternalServerError extends CustomError{
     }
     
 }
+
+export class UnauthorizedError extends CustomError {
+    statusCode = 401;
+
+    constructor(public message: string) {
+        super(message);
+        Object.setPrototypeOf(this, UnauthorizedError.prototype);
+    }
+
+    serializeError() {
+        return [{ message: this.message }];
+    }
+}
+
+export class InvalidTokenError extends CustomError {
+    statusCode = 403;
+
+    constructor(public message: string) {
+        super(message);
+        Object.setPrototypeOf(this, InvalidTokenError.prototype);
+    }
+
+    serializeError() {
+        return [{ message: this.message }];
+    }
+}
