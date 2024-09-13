8ece247a89dcc49c952a350050d1d96c1e7fe3c7
Jansiya jahan <jansiyajahan8@gmail.com>
2024-09-13 11:06:12 +0530

    feat : seperate server and app

--- server.ts
+++ server.ts
@@ -1,9 +1,20 @@
-import express from 'express';
+
 import dotenv from'dotenv';
+import { connectDB } from './backend/frameworks/config/db';
+import createApp from './backend/app';
+import { scheduleUserCleanup } from './backend/frameworks/services/cleanUpServices';
+
 
 dotenv.config()
-const app =express();
-app.listen(process.env.PORT,()=>{
-    console.log("server start");
-    
-})
+const app =createApp();
+
+connectDB().then(()=>{
+ 
+    scheduleUserCleanup();
+  const port = process.env.PORT 
+    app.listen(port, () => {
+        console.log(`Server started on port ${port}`);
+      });
+    }).catch((err) => {
+      console.error('Failed to connect to the database:', err);
+    });
