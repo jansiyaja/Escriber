b8bd6201942b0aa3d7ae18241d36ff17ce56e68e
Jansiya jahan <jansiyajahan8@gmail.com>
2024-09-13 11:17:16 +0530

    feat(resend-otp): new  resend-otp usecases is added

--- backend/application/useCases/UserUseCases.ts
+++ backend/application/useCases/UserUseCases.ts
@@ -1,72 +1,166 @@
-import { IUserRepository } from "../repositories/IUserRepository";
+import { IUserRepository } from "../../interfaces/repositories/IUserRepository";
 import { IUser } from "../../enitites/User";
-import UserModel from "../../frameworks/database/models/userModel";
-import { HashService } from "../../frameworks/services/hasServices";
-import { IEmailService } from "../repositories/IEmailRepository";
+import {HashService} from "../../frameworks/services/hashServices";
+import { IEmailService } from "../../interfaces/repositories/IEmailRepository";
 import crypto from 'crypto';
-import { IOTPVerificationRepository } from "../repositories/IOTPVerificationRepo";
+import { IOTPVerificationRepository } from "../../interfaces/repositories/IOTPVerificationRepo";
+import { generateAccessToken, generateRefreshToken } from "../../frameworks/services/jwtServices";
+import jwt from 'jsonwebtoken';
+import { IUserUseCase } from "../../interfaces/usecases/IUserCase";
+import { logger } from "../../frameworks/services/logger";
+import { BadRequestError, InvalidTokenError } from "../../frameworks/errors/BadRequestError";
 
-export class UserRegisterUseCase {
+
+
+export class UserUseCase  implements IUserUseCase{
     constructor(
-        private userRepository: IUserRepository,
-        private hashService: HashService,
-        private emailServices: IEmailService
+        private _userRepository: IUserRepository,
+        private _hashService: HashService,
+        private _emailServices: IEmailService,
+        private _otpRepository: IOTPVerificationRepository
     ) {}
 
-    async execute(userData: Partial<IUser>): Promise<IUser> {
-        const existingUser = await this.userRepository.findByEmail(userData.email!);
+    async registerUser(userData: Partial<IUser>):Promise<{ user: IUser; accessToken: string; refreshToken: string }> {
+        
+        logger.info('checking existing user')
+
+        const existingUser = await this._userRepository.findByEmail(userData.email!);
+        console.log(existingUser);
+        
         if (existingUser) {
-            throw new Error("User with email already exists");
+           throw new  BadRequestError("User with email already exists")
+            
         }
         
-        const hashedPassword = await this.hashService.hash(userData.password!);
-        const otp = crypto.randomInt(1000, 9999).toString(); 
-
-        const newUser = new UserModel({
-            ...userData,
-            password: hashedPassword
-        });
-
-        const createUser = await this.userRepository.create(newUser);
+        const hashedPassword = await this._hashService.hash(userData.password!);
+        const otp = crypto.randomInt(1000, 9999).toString();
+        console.log(otp);
         
-        await this.emailServices.sendEmail({
+
+        const newUser: IUser = {
+            ...userData,          
+            password: hashedPassword, 
+            isVerified: false    
+        } as IUser;
+
+        const createUser = await this._userRepository.create(newUser);
+
+        if (!createUser.email) {
+            throw new Error("Failed to create user: Email is null");
+        }
+
+        const newOtp = { otp: otp, email: createUser.email };
+        await this._otpRepository.create(newOtp);
+
+        await this._emailServices.sendEmail({
             to: userData.email!,
             subject: "Welcome To Escriber, Our Blog Platform!",
-            text: `Hi ${userData.username}, welcome to our platform! We're excited to have you here.Your Otp Is ${otp}` 
+            text: `Hi ${userData.username}, welcome to our platform! We're excited to have you here.Your Otp Is ${otp}`
         });
-        
-        return createUser;
+
+        const accessToken = generateAccessToken(createUser._id!);
+        const refreshToken = generateRefreshToken(createUser._id!);
+
+        return {
+            user: createUser,
+            accessToken,
+            refreshToken
+        };
     }
-}
 
-export class VerifyOTPUseCase {
-    constructor(
-        private userRepository: IUserRepository,
-        private otpVerificationRepository: IOTPVerificationRepository
-    ) {}
+    async verifyOTP({ otp, email }: { otp: string; email: string }): Promise<boolean> {
 
-    async execute({ otp, email }: { otp: string; email: string }): Promise<boolean> {
-        console.log('Verifying OTP for user ID:', email);
+       logger.info('Verifying OTP for email', { email });
         
-        const otpVerification = await this.otpVerificationRepository.findByUserByEmail(email);
+        const otpVerification = await this._otpRepository.findByUserByEmail(email);
         
         if (!otpVerification) {
-            console.error('No OTP record found for user ID:', email);
-            throw new Error('No OTP record found');
+            throw new BadRequestError("No OTP record found for this email");
         }
-
-        console.log('Retrieved OTP Verification record:', otpVerification);
 
         if (otpVerification.otp !== otp) {
-            console.error('Invalid OTP provided:', otp);
-            throw new Error('Invalid OTP');
+            throw new BadRequestError("Invalid OTP provided");
         }
 
-        await this.otpVerificationRepository.deleteByUserId(email);
-        await this.userRepository.markAsVerified(email);
+
+        await this._userRepository.markAsVerified(email);
+        await this._otpRepository.deleteByUserId(email);
 
         return true;
     }
+
+    async loginUser(userData: Partial<IUser>): Promise<{ user: IUser; accessToken: string; refreshToken: string }> {
+        const existingUser = await this._userRepository.findByEmail(userData.email!);
+        if (!existingUser) {
+            throw new Error("User with email not exists");
+        }
+        
+        const isMatch = await this._hashService.compare(userData.password!, existingUser.password!);
+
+        if (!isMatch) {
+            throw new Error("Invalid password");
+        }
+           
+        const userId = existingUser._id;
+        if (!userId) {
+          throw new Error("User ID is missing");
+        }
+          const accessToken = generateAccessToken(userId);
+          const refreshToken = generateRefreshToken(userId);
+          
+          
+
+         return{
+            user:existingUser,
+            accessToken:accessToken,
+            refreshToken:refreshToken
+         }
+    }
+
+    async verifyToken(token: string): Promise<{ accessToken: string; refreshToken: string }> {
+        try {
+            logger.info('Starting token verification');
+            const decoded = jwt.verify(token, process.env.REFRESH_TOKEN_SECRET as string) as { userId: string };
+
+            const user = await this._userRepository.findById(decoded.userId);
+            
+            if (!user || !user._id) {
+                throw new Error('User not found or user ID is missing');
+            }
+
+            const accessToken = generateAccessToken(user._id);
+            const refreshToken = generateRefreshToken(user._id);
+
+            return {
+                accessToken,
+                refreshToken
+            }
+        } catch (error) {
+            console.error('Error verifying token:', error);
+            throw new Error('Invalid or expired refresh token');
+        }
+    }
+    async resendOTP({ email }: { email: string; }): Promise<void> {
+              const otp = crypto.randomInt(1000, 9999).toString();
+
+              console.log("new OTP",otp );
+              console.log("new Email",email );
+              
+              await this._otpRepository.updateOtp(email, otp);
+
+             const userData= await this._userRepository.findByEmail(email)
+              console.log(userData,"user");
+              
+            
+             if(!userData){
+                 throw new InvalidTokenError('There is no user  with this email')
+             }
+
+              await this._emailServices.sendEmail({
+                  to: userData.email!,
+                  subject: "Welcome To Escriber, Our Blog Platform!",
+                  text: `Hi ${userData.username}, welcome to our platform! We're excited to have you here.Your Otp Is ${otp}`
+              });
+      
+    }
 }
-
-
